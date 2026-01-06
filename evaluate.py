0084e8372a1d1411840b0e3740c589c4b3e2f953
sadlavaarsc <sadlavaarsc3248@gmail.com>
2026-01-06 21:48:06 +0800

    修复文件名修改导致的引用问题

--- evaluate.py
+++ evaluate.py
@@ -16,7 +16,7 @@ evaluate.py - Agent 评估脚本
 from utils import set_random_seed
 from poolenv import PoolEnv
 from agent import BasicAgent
-from train import PPOAgent
+from train_ppo import PPOAgent
 
 # 设置随机种子，enable=True 时使用固定种子，enable=False 时使用完全随机
 # 根据需求，我们在这里统一设置随机种子，确保 agent 双方的全局击球扰动使用相同的随机状态
